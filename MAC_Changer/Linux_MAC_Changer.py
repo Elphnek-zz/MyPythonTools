4ced9abe590129d27995b9ff25a7428c5d7e7d99
elphnek <elphnekity@gmail.com>
2020-08-13 19:23:22 +0300

    Changed the Linux_MAC_Changer.py & Win_MAC_Changer.py, added functions, option parser and returned values

--- MAC_Changer/Linux_MAC_Changer.py
+++ MAC_Changer/Linux_MAC_Changer.py
@@ -1,2 +1,33 @@
-import subprocess
-subprocess.run('ifconfig eth1 hw ether 00:11:22:33:44:55', shell=True)
+#!/usr/bin/env python3
+
+import subprocess 
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        #code to execute
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        #code to execute
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    return options
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for: " + interface +" to: "  + new_mac)
+
+    #Secure Way to call subprocess:
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
+
+# Not Secure Way to call subprocess:
+# subprocess.call("ifconfig "+interface+" down", shell=True)
+# subprocess.call("ifconfig "+interface+" hw ether "+new_mac, shell=True)
+# subprocess.call("ifconfig "+interface+" up", shell=True)
--- MAC_Changer/Win_MAC_Changer.py
+++ MAC_Changer/Win_MAC_Changer.py
@@ -1,4 +1,26 @@
-import subprocess
+#!/usr/bin/env python3
 
-subprocess.call(["C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe", "Set-NetAdapter -Name 'Ethernet' -MacAddress '00:11:22:33:44:55'"])
+import subprocess 
+import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        #code to execute
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        #code to execute
+        parser.error("[-] Please specify a new mac, use --help for more info")
+    return options
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for: " + interface +" to: "  + new_mac)
+
+    #Secure Way to call subprocess:
+    subprocess.call(["C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe", "Set-NetAdapter", "-name", interface, "-macaddress", new_mac]) 
+    
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
